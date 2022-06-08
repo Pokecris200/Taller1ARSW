08fed346d39aea0018322b2e0f9bf665f12cb66c
Cristian Forero <cristian.forero-m@mail.escuelaing.edu.co>
2022-06-08 16:16:14 -0500

    Funcionalidad de lectura de lineas de codigo Implementada

--- miprimera-app/src/main/java/edu/escuelaing/arsw/app/IOLector.java
+++ miprimera-app/src/main/java/edu/escuelaing/arsw/app/IOLector.java
@@ -10,17 +10,30 @@ public class IOLector
 {
     public static void main( String[] args ) throws Exception
     {
-    	System.out.println( args[0] );
-    	String path = args[0];
-    	path.replace( "\","\\");
-        File file = new File( path );
+    	System.out.println( args[1] );
+        File file = new File( args[1] );
         Scanner scan = new Scanner(file);
-
+        
         int length = 0;
-        while(scan.hasNextLine()) 
+        if (args[0].toUpperCase().equals("PHY"))
         {
-        	scan.nextLine();
-        	length++;
+        	while(scan.hasNextLine()) 
+            {
+            	scan.nextLine();
+            	length++;
+            }
+        }
+        else if(args[0].toUpperCase().equals("LOC"))
+        {
+        	while(scan.hasNextLine()) 
+            {
+            	String line = scan.nextLine();
+            	Boolean condition = line.contains("/*") || line.contains("//") || line.contains("*/") || line.equals("");
+            	if(condition)
+            	{
+            		length++;
+            	}
+            }
         }
         System.out.println( length );
     }
